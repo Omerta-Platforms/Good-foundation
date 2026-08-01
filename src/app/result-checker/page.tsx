b6a06e85690624544ef2db5b481cc6a422a86080
Omerta <theoriginplatforms@gmail.com>
2026-08-01 11:58:06 +0100

    Update page.tsx

--- src/app/result-checker/page.tsx
+++ src/app/result-checker/page.tsx
@@ -509,6 +509,19 @@ export default function ResultCheckerPage() {
           </p>
           <p className="text-xs text-gray-500 mt-1">Knowledge for Progress</p>
         </div>
+
+        <div className="flex flex-col items-center justify-center space-y-3 mt-4">
+          <div className="border-t border-gray-800 mt-4 pt-6 text-center text-sm text-gray-400">
+            powered by
+          </div>
+            <Image
+              src="/omerta.png"
+              alt="origin"
+              width={100}
+              height={30}
+              className="object-contain"
+          />
+        </div>
       </footer>
     </div>
   )
